0feae7592e4fa3220b40a859d487d5eac0115533
Arthur Guimarães <arthurdonds@gmail.com>
2023-08-07 10:14:41 -0300

    Initial commit

---
+++ TestApiOnLambda/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hello Tuts from AWS Lambda!");
+
+app.Run();
